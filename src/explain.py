385e14334559667f80e8c5e96a2f5ebe5b6f88c7
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2025-01-26 15:24:34 +0100

    refactored explain.py

--- src/explain.py
+++ src/explain.py
@@ -1,6 +1,7 @@
 # deep learning libraries
 import torch
 from torch.jit import RecursiveScriptModule
+from torch.utils.data import DataLoader
 
 # other libraries
 from typing import Final
@@ -22,6 +23,23 @@ batch_size: int = 16
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 
+def calculate_saliency_map(
+    model: torch.nn.Module,
+    dataloader: DataLoader,
+) -> None:
+    # Freeze parameters
+    freeze_model(model)
+
+    # get a batch
+    inputs, targets = next(iter(dataloader))
+
+    max_indices = compute_gradients_input(model, inputs, device)
+
+    show_saliency_map_grid(
+        inputs.detach().cpu(), inputs.grad, targets, max_indices, ncol=4
+    )
+
+
 def main() -> None:
     train_data, val_data, test_data = load_data(DATA_PATH, batch_size=batch_size)
     name: str = "scheduler_run"
@@ -33,17 +51,8 @@ def main() -> None:
     accuracy: float = test_step(model, test_data, device)
     print(f"Model test accuracy: {accuracy}")
 
-    # Freeze parameters
-    freeze_model(model)
-
-    # get a batch
-    inputs, targets = next(iter(train_data))
-
-    max_indices = compute_gradients_input(model, inputs, device)
-
-    show_saliency_map_grid(
-        inputs.detach().cpu(), inputs.grad, targets, max_indices, ncol=4
-    )
+    # calculate saliency map
+    calculate_saliency_map(model, train_data)
 
 
 if __name__ == "__main__":
